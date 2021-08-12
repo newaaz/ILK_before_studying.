f744bbe862f6a61ec26c2ee70ada8beb697f2b4c
aaz <ilk-ui@yandex.ru>
2021-08-12 16:19:47 +0300

    коммит для тест-VPS - сохраняющий изменение в конфиге деплоя (ENV[branch...])

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,8 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
 
+set :branch, ENV['BRANCH'] if ENV['BRANCH']
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
